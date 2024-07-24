18beaa9ff93fc9582df5d26be56f97aabb955b39
Patrick Chaves <patrickchaves@MacBook-Air-de-Patrick.local>
2024-07-24 20:38:17 -0300

    [fix] fixing public-api exporting

--- lib/formuverse/src/public-api.ts
+++ lib/formuverse/src/public-api.ts
@@ -4,4 +4,4 @@
 
 export * from './lib/core/interfaces/data.interface';
 export * from './lib/formuverse.component';
-export * from './lib/core/enums/form.enum';
+export { CriteriaEnum, FormFieldMultipleTypeEnum, FormFieldSingleTypeEnum, FormFieldType } from './lib/core/enums/form.enum';
