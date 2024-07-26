7f0e06fbb96e723d1daa1249b816985b7e649dfd
Patrick Chaves <patrickchaves@MacBook-Air-de-Patrick.local>
2024-07-25 22:23:51 -0300

    [fix] changing multiplefields FormArray to FormGroup for better results

--- lib/formuverse/src/lib/core/services/form.service.ts
+++ lib/formuverse/src/lib/core/services/form.service.ts
@@ -1,8 +1,9 @@
-import { ValidatorFn } from '@angular/forms';
+import { FormGroup, ValidatorFn } from '@angular/forms';
 import {
     FormControlsInterface,
     FormDataInterface,
     FormFieldMultipleInterface,
+    FormFieldOptionInterface,
     FormGroupInterface,
     FormType,
     FormuverseAbstractControl,
@@ -11,43 +12,47 @@ import { FormBase } from '../class/formBase';
 import { Injectable } from '@angular/core';
 import { FormFieldMultipleTypeEnum } from '../enums/form.enum';
 import { FormuverseControl } from '../class/formControl';
-import { FormuverseArray } from '../class/formArray';
+import { BehaviorSubject } from 'rxjs';
+import { FormuverseGroup } from '../class/formGroup';
 
 @Injectable({
     providedIn: 'root'
   })
 export class FormService {
-    createForm<T extends Record<string, any>>(
+    async createForm<T extends Record<string, any>>(
         data: FormDataInterface | FormGroupInterface
-    ): FormType<T> {
-        return new FormBase<T>(this._createControls<T>(data), data) as FormType<T>;
+    ): Promise<FormType<T>> {
+        return new FormBase<T>(await this._createControls<T>(data), data) as FormType<T>;
     }
 
-    private _createControls<T>(
+    private async _createControls<T extends Record<string, any>>(
         data: FormDataInterface | FormGroupInterface
-    ): FormControlsInterface<T> {
+    ) {
         const allControls: FormControlsInterface<T> =
             {} as FormControlsInterface<T>;
         if (data.hasOwnProperty('fields')) {
             data = data as FormGroupInterface;
-            this._createFromGroup(data).forEach((control, index) => {
+            const formGroup = await this._createFromGroup(data)
+            formGroup.forEach((control, index) => {
                 allControls[
                     (data as FormGroupInterface).fields[index].name as keyof T
                 ] = control;
             });
         } else if (data.hasOwnProperty('groups')) {
             data = data as FormDataInterface;
-            data.groups.forEach((group) =>
-                this._createFromGroup(group).forEach((control, index) => {
+            data.groups.forEach(async (group) => {
+                const formGroup = await this._createFromGroup(group)
+                formGroup.forEach((control, index) => {
                     allControls[group.fields[index].name as keyof T] = control;
                 })
+            }
             );
         }
         return allControls;
     }
 
-    private _createFromGroup(group: FormGroupInterface): FormuverseAbstractControl<any>[] {
-        return group.fields.map((field) => {
+    private _createFromGroup(group: FormGroupInterface): Promise<FormuverseAbstractControl<any>[]> {
+        const promises = group.fields.map(async (field) => {
             const validators: ValidatorFn[] = []
             if(field.validators){
                 for (const validator in field.validators) {
@@ -57,18 +62,29 @@ export class FormService {
             if ([FormFieldMultipleTypeEnum.CHECKBOX, FormFieldMultipleTypeEnum.MULTISELECT].includes(field.type as FormFieldMultipleTypeEnum)
             ) {
                 field = field as FormFieldMultipleInterface;
-                const options =
-                    field.options instanceof Array
-                        ? field.options.map(
-                              (option) => new FormuverseControl(option.value)
-                          )
-                        : [];
-                return new FormuverseArray(
-                    options,
-                    validators
-                );
+                return await this._createFormGroupForOptions(field.options, validators);
             }
             return new FormuverseControl<any>(field.initialValue, validators);
         });
+        return Promise.all(promises);
+    }
+
+    private _createFormGroupForOptions(options: FormFieldOptionInterface[] | BehaviorSubject<FormFieldOptionInterface[]>, validators: ValidatorFn[]): Promise<FormuverseGroup> {
+        return new Promise<FormuverseGroup>((resolve, reject) => {
+            if (options instanceof BehaviorSubject) {
+                options.subscribe((options) => {
+                    return resolve(new FormuverseGroup(this._createFormControlsForOptions(options), validators));
+                });
+            } else {
+                resolve(new FormuverseGroup(this._createFormControlsForOptions(options)));
+            }
+
+        });
+    }
+
+    private _createFormControlsForOptions(options: FormFieldOptionInterface[]) {
+        const formControls: Record<string, FormuverseControl<boolean>> = {};
+        options.forEach((option) => (formControls[option.value] = new FormuverseControl(option.value)))
+        return formControls;
     }
 }
